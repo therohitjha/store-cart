b36786e8e9e8e927f65cf13e18c976307f11912f
Rohit <therohitjha@gmail.com>
2020-04-30 16:40:23 +0530

    Updates

---
+++ precache-manifest.8f1ce4287237ca88e48da47894a2c42e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014541222fa9b2e90fe16e6f845c0dd4",
+    "url": "/store-cart/index.html"
+  },
+  {
+    "revision": "7577c301876cf70cb8d1",
+    "url": "/store-cart/static/css/2.ed4a80a4.chunk.css"
+  },
+  {
+    "revision": "35d09ecc2803ffe6123a",
+    "url": "/store-cart/static/css/main.742aa3fd.chunk.css"
+  },
+  {
+    "revision": "7577c301876cf70cb8d1",
+    "url": "/store-cart/static/js/2.61fc7011.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/store-cart/static/js/2.61fc7011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d09ecc2803ffe6123a",
+    "url": "/store-cart/static/js/main.581c07fb.chunk.js"
+  },
+  {
+    "revision": "009c60a51944dc1cae6f",
+    "url": "/store-cart/static/js/runtime-main.6c9edcca.js"
+  }
+]);
